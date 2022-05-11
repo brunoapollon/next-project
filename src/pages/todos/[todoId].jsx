647d31777b0c011dd5706dfd60ce74415446ea66
Bruno Lopes <eusoubruno7@gmail.com>
2022-05-10 21:48:47 -0300

    fazendo a pre redenrização de páginas com dados de uma api

--- src/pages/todos/[todoId].jsx
+++ src/pages/todos/[todoId].jsx
@@ -1,21 +1,46 @@
 import Link from "next/link";
 
-import { useRouter } from "next/router";
+export async function getStaticProps(context) {
+  const { params } = context;
 
-export default function Todo() {
-  const router = useRouter();
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${params.todoId}`
+  );
 
-  const { todoId } = router.query;
+  const todo = await data.json();
 
+  return {
+    props: { todo },
+  };
+}
+
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+
+  const data = await response.json();
+
+  const paths = data.map((todo) => {
+    return {
+      params: {
+        todoId: `${todo.id}`,
+      },
+    };
+  });
+
+  return { paths, fallback: false };
+}
+
+export default function Todo({ todo }) {
   return (
     <>
       <Link href="/">
         <a>Voltar</a>
       </Link>
-      <h1>Exibindo o todo {todoId}</h1>
+      <h1>Exibindo o todo {todo.id}</h1>
+      <strong>descrição {todo.title}</strong>
       <p>
         Comentario aaaaaaaaa{" "}
-        <Link href={`/todos/${todoId}/comments/1`}>
+        <Link href={`/todos/${todo.id}/comments/1`}>
           <a>Infos</a>
         </Link>
       </p>
